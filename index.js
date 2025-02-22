b0a3b64b8b66fe78b344b4da5ef350b6b82921b4
sankethanse <120187322+sohamhanse@users.noreply.github.com>
2025-02-22 21:00:35 +0530

    Root Response added

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
+app.get('/', (req, res) => {
+    res.send("Bhai!!! Well Done")
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
